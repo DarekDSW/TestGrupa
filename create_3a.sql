08775e6dbfabe162d45cab5720a33f4aa22ec965
tadeusz <tadeusz.drabarek@wp.pl>
2017-09-16 15:12:27 +0200

    Poprawka na opiekunowie oraz nowa tabela zamowienia

---
+++ create_3a.sql
@@ -0,0 +1,18 @@
+drop table opiekonowie;
+
+create table opiekunowie (
+	id_o int primary key auto_increment,
+    nazwa text,
+    -- id_p int,
+    id_k int,
+    -- foreign key (id_p) references produkty(id_p),
+    foreign key (id_k) references klienci(id_k)
+);
+
+create table zamowienia(
+	id_z int primary key auto_increment,
+    id_o integer,
+    id_p integer,
+    foreign key (id_p) references produkty(id_p),
+    foreign key (id_o) references opiekunowie(id_o)
+);
